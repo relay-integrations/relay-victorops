4f939bc84a851ba5bfa803ad5c47eb922340ec95
Eric Sorenson <eric.sorenson@puppetlabs.com>
2020-10-07 11:57:15 -0700

    Make it more generic rather than expecting specific fields

--- triggers/incident-triggered/handler.py
+++ triggers/incident-triggered/handler.py
@@ -12,13 +12,14 @@ logging.getLogger().setLevel(logging.INFO)
 async def handler():
     data = await request.form
 
-    relay.events.emit({
-	'incident_id': data['incident'],
-	'incident_name': data['entity_display_name'],
-	'incident_state': data['entity_state'],
-	'incident_routing_key': data['routing_keys'],
-	'incident_entity_id': data['entity_id']
-    })
+    event = {}
+
+    for field, value in data.items():
+        event[field] = value
+
+    relay.events.emit(event)
+
+
     return await render_template_string("Relay received payload about {{incident_id}}", incident_id=data['incident'])
 
 if __name__ == '__main__':
